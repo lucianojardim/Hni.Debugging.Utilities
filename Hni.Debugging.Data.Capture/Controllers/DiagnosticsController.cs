f42d04ed3d4f2c59e2ef50beb4b5436b3ef2ea3e
JardimL <JardimL@WKSLT-123053>
2016-06-20 11:24:15 -0500

    Added capability to work with connection strings based on environment: development, quality assurance, or production

--- Hni.Debugging.Data.Capture/Controllers/DiagnosticsController.cs
+++ Hni.Debugging.Data.Capture/Controllers/DiagnosticsController.cs
@@ -9,12 +9,13 @@ using System.Net.Http;
 using System.Web.Http;
 using System.Web.Http.Description;
 using Hni.Debugging.Data.Capture.Models;
+using HNI.Environment;
 
 namespace Hni.Debugging.Data.Capture.Controllers
 {
     public class DiagnosticsController : ApiController
     {
-        private HniDiagnosticEntities db = new HniDiagnosticEntities();
+        private HniDiagnosticEntities db = new HniDiagnosticEntities(ConfigurationUtil.GetDeploymentValue("HniDiagnosticEntities"));
 
         // GET: api/Diagnostics
         //public IQueryable<Diagnostic> GetDiagnostics()
@@ -23,57 +24,72 @@ namespace Hni.Debugging.Data.Capture.Controllers
         //}
 
         // GET: api/Diagnostics/5
-        [ResponseType(typeof(Diagnostic))]
-        public IHttpActionResult GetDiagnostic(long id)
-        {
-            Diagnostic diagnostic = db.Diagnostics.Find(id);
-            if (diagnostic == null)
-            {
-                return NotFound();
-            }
+        //[ResponseType(typeof(Diagnostic))]
+        //public IHttpActionResult GetDiagnostic(long id)
+        //{
+        //    Diagnostic diagnostic = db.Diagnostics.Find(id);
+        //    if (diagnostic == null)
+        //    {
+        //        return NotFound();
+        //    }
 
-            return Ok(diagnostic);
-        }
+        //    return Ok(diagnostic);
+        //}
 
         // PUT: api/Diagnostics/5
-        [ResponseType(typeof(void))]
-        public IHttpActionResult PutDiagnostic(long id, Diagnostic diagnostic)
-        {
-            if (!ModelState.IsValid)
-            {
-                return BadRequest(ModelState);
-            }
+        //[ResponseType(typeof(void))]
+        //public IHttpActionResult PutDiagnostic(long id, Diagnostic diagnostic)
+        //{
+        //    if (!ModelState.IsValid)
+        //    {
+        //        return BadRequest(ModelState);
+        //    }
 
-            if (id != diagnostic.DiagnosticID)
-            {
-                return BadRequest();
-            }
+        //    if (id != diagnostic.DiagnosticID)
+        //    {
+        //        return BadRequest();
+        //    }
 
-            db.Entry(diagnostic).State = EntityState.Modified;
+        //    db.Entry(diagnostic).State = EntityState.Modified;
 
-            try
-            {
-                db.SaveChanges();
-            }
-            catch (DbUpdateConcurrencyException)
-            {
-                if (!DiagnosticExists(id))
-                {
-                    return NotFound();
-                }
-                else
-                {
-                    throw;
-                }
-            }
+        //    try
+        //    {
+        //        db.SaveChanges();
+        //    }
+        //    catch (DbUpdateConcurrencyException)
+        //    {
+        //        if (!DiagnosticExists(id))
+        //        {
+        //            return NotFound();
+        //        }
+        //        else
+        //        {
+        //            throw;
+        //        }
+        //    }
 
-            return StatusCode(HttpStatusCode.NoContent);
-        }
+        //    return StatusCode(HttpStatusCode.NoContent);
+        //}
 
         // POST: api/Diagnostics
         [ResponseType(typeof(Diagnostic))]
         public IHttpActionResult PostDiagnostic(Diagnostic diagnostic)
         {
+            if (diagnostic.DiagnosticSource == null)
+            {
+                diagnostic.DiagnosticSource = "The source didn't identified itself.";
+            }
+
+            if (diagnostic.DiagnosticData == null)
+            {
+                diagnostic.DiagnosticData = "No diagnostic data was sent by the source.";
+            }
+
+            if(diagnostic.DiagnosticDateTime == null)
+            {
+                diagnostic.DiagnosticDateTime = System.DateTime.Now;
+            }
+
             if (!ModelState.IsValid)
             {
                 return BadRequest(ModelState);
@@ -110,9 +126,9 @@ namespace Hni.Debugging.Data.Capture.Controllers
             base.Dispose(disposing);
         }
 
-        private bool DiagnosticExists(long id)
-        {
-            return db.Diagnostics.Count(e => e.DiagnosticID == id) > 0;
-        }
+        //private bool DiagnosticExists(long id)
+        //{
+        //    return db.Diagnostics.Count(e => e.DiagnosticID == id) > 0;
+        //}
     }
 }
